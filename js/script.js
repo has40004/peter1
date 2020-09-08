2b7200ef766edebc9aa979e5fc30526d26f7f223
hasan <alsamman@mail.ru>
2020-09-08 15:56:13 +0300

    js

---
+++ js/script.js
@@ -0,0 +1,29 @@
+"use strict";
+// const result = console.log(confirm ('hello', ''));
+// const answer = prompt ('haw are you?' , 'hi');
+// console.log(answer);
+// const answer = [];
+// answer [0] = prompt ('как ваше имя?', '');
+// answer [1] = prompt ('как ваша фамилия?', '');
+// answer [2] = prompt ('сколько вам лет?', '');
+// document.write(answer);
+let a = 10,
+    b =5;
+    ++a;
+    ++b;
+console.log(a);
+console.log(b);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
