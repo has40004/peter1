a7026045edecae567feea98a6945f85e8963dfa9
hasan <alsamman@mail.ru>
2020-09-08 16:47:37 +0300

    home work 9

--- js/script.js
+++ js/script.js
@@ -7,13 +7,28 @@
 // answer [1] = prompt ('как ваша фамилия?', '');
 // answer [2] = prompt ('сколько вам лет?', '');
 // document.write(answer);
-let a = 10,
-    b =5;
-    ++a;
-    ++b;
-console.log(a);
-console.log(b);
+// let a = 10,
+//     b =5;
+//     ++a;
+//     ++b;
+// console.log(a);
+// console.log(b);
+const numberOfFilms = +prompt ("Сколько фильмов вы уже просмотрели?" , "");
+const personalMovieDB = {
+    count: numberOfFilms,
+    Movies: {},
+    actors: {},
+    genres:[],
+    privat: false
+};
+const a = prompt ("один из последних просмотренных фильмов?" , ""),
+      b = prompt ("на сколько оцените его ?" , ""),
+      c = prompt ("один из последних просмотренных фильмов?" , ""),
+      d = prompt ("на сколько оцените его ?" , "");
 
+personalMovieDB.Movies[a] = b; 
+personalMovieDB.Movies[c] = d;
+console.log(personalMovieDB);
 
 
 
